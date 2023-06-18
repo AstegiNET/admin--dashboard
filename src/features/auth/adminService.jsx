031a6b853b5695aaedb11e7b6044277a0487c05e
kaleabbyh <kaleabbyh@gmail.com>
2023-06-18 17:31:44 +0300

    new update

--- src/features/auth/adminService.jsx
+++ src/features/auth/adminService.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
-
+import { LOGIN_ADMIN } from "../../api/API";
 const API_URL = "http://localhost:5000/api/admins/login";
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL, userData);
+  const response = await axios.post(LOGIN_ADMIN, userData);
 
   if (response.data) {
     localStorage.setItem("admin", JSON.stringify(response.data));
