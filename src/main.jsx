d22ee4e7730632569d419518ae0e86981e0cb393
Kaleab Bayih <kaleabbyh@gmail.com>
2023-07-20 09:40:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import "./index.css";
 
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-// import { store } from "./store/store";
-// import { Provider } from "react-redux";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
